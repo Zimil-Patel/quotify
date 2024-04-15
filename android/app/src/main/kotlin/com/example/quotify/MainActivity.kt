57498e4b5a8027178244f5422f7b71125295f910
Zimil-Patel <patelzimil2310@gmail.com>
2024-04-15 20:19:09 +0530

    Initial commit of project

---
+++ android/app/src/main/kotlin/com/example/quotify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quotify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const QuotifyApp());
+}
+
+
+class QuotifyApp extends StatelessWidget {
+  const QuotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
