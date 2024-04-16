5512173f56cdc1f276fc1dc345e798b3d77e9c18
Zimil-Patel <patelzimil2310@gmail.com>
2024-04-16 20:55:06 +0530

    base of app 2

--- lib/utils/theme/dark_theme.dart
+++ lib/utils/theme/dark_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-  scaffoldBackgroundColor: const Color(0xff22252a),
+  scaffoldBackgroundColor: const Color(0xff242d3c),
   brightness: Brightness.dark,
   //TEXT DATA
   textTheme: const TextTheme(
@@ -15,10 +15,15 @@ ThemeData darkTheme = ThemeData(
         color: CupertinoColors.white,
         fontWeight: FontWeight.w300), //FOR CATEGORY NAME
     bodyMedium: TextStyle(
-        color: CupertinoColors.white, fontWeight: FontWeight.w700), //FOR YOU
+        color: CupertinoColors.white, fontWeight: FontWeight.w700),
+    bodySmall: TextStyle(
+        color: Color(0xffd4d6dc), fontWeight: FontWeight.w700),
+    //FOR YOU
   ),
   colorScheme: const ColorScheme.dark(
     primary: Color(0xff242d3c),
+    secondary: CupertinoColors.white,
     surface: Color(0xff344050),
+    onSurface: Colors.white,
   ),
 );
--- lib/view/home screen/home_screen.dart
+++ lib/view/home screen/home_screen.dart
@@ -14,24 +14,6 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                boxShadow: [
-                  BoxShadow(
-                    spreadRadius: 1,
-                    blurRadius: 14,
-                    color: Theme.of(context).colorScheme.shadow
-                  )
-                ]
-              ),
-              child: ListTile(
-                title: Text('Title', style: Theme.of(context).textTheme.titleLarge,),
-                subtitle: Text('Sub title', style: Theme.of(context).textTheme.bodyMedium,),
-                leading: Icon(Icons.ac_unit, color: Theme.of(context).colorScheme.onSurface,),
-              ),
-            ),
           ],
         ),
       ),
