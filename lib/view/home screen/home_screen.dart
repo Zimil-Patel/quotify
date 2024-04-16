014ab263be565a67c972a7438a00cfdc7db77ccb
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-04-16 21:52:30 +0530

    akshar

--- lib/view/home screen/home_screen.dart
+++ lib/view/home screen/home_screen.dart
@@ -16,14 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Column(
               children: [
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
-                Text('hello'),
+                Text('hello')
               ],
             )
           ],
