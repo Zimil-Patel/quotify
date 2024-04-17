b9dc1a7515bc5b8b38994c656cee4cb9eeecd9c1
Dipali Gunjal <dipaligunjal971@gmail.com>
2024-04-17 10:02:45 +0530

    from dipali

--- lib/utils/theme/dark_theme.dart
+++ lib/utils/theme/dark_theme.dart
@@ -1,29 +1,31 @@
-//DARK THEME DATA
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//DARK THEME DATA
 ThemeData darkTheme = ThemeData(
-  scaffoldBackgroundColor: const Color(0xff242d3c),
+  scaffoldBackgroundColor: const Color(0xff22252a),
   brightness: Brightness.dark,
   //TEXT DATA
   textTheme: const TextTheme(
-    titleMedium: TextStyle(
-        color: Color(0xff141b25), fontWeight: FontWeight.bold), //FOR BOX TITLE
-    titleLarge: TextStyle(
-        color: CupertinoColors.white, fontWeight: FontWeight.w900), // FOR TITLE
-    titleSmall: TextStyle(
-        color: CupertinoColors.white,
-        fontWeight: FontWeight.w300), //FOR CATEGORY NAME
+    displayLarge: TextStyle(
+        fontSize: 12,
+        color: Colors.blue
+    ),
     bodyMedium: TextStyle(
-        color: CupertinoColors.white, fontWeight: FontWeight.w700),
-    bodySmall: TextStyle(
-        color: Color(0xffd4d6dc), fontWeight: FontWeight.w700),
-    //FOR YOU
+          color: Color(0xff9d9fa1)
+      ),
+    titleLarge: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w900
+    ),
   ),
+
+
   colorScheme: const ColorScheme.dark(
-    primary: Color(0xff242d3c),
-    secondary: CupertinoColors.white,
-    surface: Color(0xff344050),
-    onSurface: Colors.white,
+    primary: Color(0xff22252a),
+    secondary: Color(0xff2d3035),
+    surface: Color(0xff3d3f44),
+    onSurface: Colors.black,
+    shadow: Colors.black26,
   ),
 );
+
---
+++ lib/view/profile screen/general_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GeneralScreen extends StatefulWidget {
+  const GeneralScreen({super.key});
+
+  @override
+  State<GeneralScreen> createState() => _GeneralScreenState();
+}
+
+class _GeneralScreenState extends State<GeneralScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
