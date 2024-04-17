7bed86e9544e6af1ce6bd5fe6974b2c6fe312580
Zimil-Patel <patelzimil2310@gmail.com>
2024-04-17 15:35:59 +0530

    master push

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:quotify/utils/theme/dark_theme.dart';
 import 'package:quotify/utils/theme/light_theme.dart';
+import 'package:quotify/utils/theme/theme_provider.dart';
 import 'package:quotify/view/home%20screen/home_screen.dart';
 
 void main(){
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.black12, // navigation bar color
     statusBarColor: Colors.black12, // status bar color
   ));
-  runApp(const QuotifyApp());
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider(),)
+      ],
+      child: const QuotifyApp()));
 }
 
 
@@ -18,11 +24,12 @@ class QuotifyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ThemeProvider themeProviderTrue = Provider.of(context, listen: true);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: themeProviderTrue.getThemeMode(),
       home: const HomeScreen(),
     );
   }
