1ea9cb4954d8b65f3adae4237c8bee6a43d850a7
Zimil-Patel <patelzimil2310@gmail.com>
2024-04-18 10:55:57 +0530

    master push

--- lib/view/home screen/home_screen.dart
+++ lib/view/home screen/home_screen.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:quotify/utils/global_variables.dart';
+
+
+import 'components/ bottom_button_box.dart';
+import 'components/bottom_navigation.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,13 +19,64 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text('ayush patel')
-          ],
-        ),
+      //backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Stack(
+        children: [
+          PageView(
+            reverse: true,
+            padEnds: false,
+            scrollDirection: Axis.vertical,
+            children: List.generate(
+                reel.length,
+                (index) => Container(
+                      height: height,
+                      width: width,
+                      color: reel[index],
+                    )),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 150),
+            child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: Icon(
+                        CupertinoIcons.heart,
+                        size: 55,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 100),
+                      child: Icon(
+                        CupertinoIcons.share,
+                        size: 55,
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: bottomNavigationBox(context)),
+          // buildSizedBox(
+          //   h1: 100,
+          // ),
+        ],
       ),
     );
   }
 }
+
+
+List<Color> reel = [
+  Colors.green,
+  Colors.red,
+  Colors.blue,
+  Colors.yellow,
+  Colors.grey,
+  Colors.blueGrey,
+  Colors.purple,
+];
