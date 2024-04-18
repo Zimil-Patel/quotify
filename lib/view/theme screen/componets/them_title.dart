4757dadf6765c8042439499945c8ba83110fcbdf
Fenish Patel <patelfenish34@gmail.com>
2024-04-18 15:24:51 +0530

    18-04-2024

---
+++ lib/view/theme screen/componets/them_title.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:quotify/utils/global%20list/theme_list.dart';
+import 'package:quotify/utils/global_variables.dart';
+
+Widget ThemeTitle(BuildContext context) {
+  return Row(
+    children: [
+      SizedBox(width: 20,),
+      Icon(CupertinoIcons.clear),
+      SizedBox(width: 20,),
+      Text('Themes',style:Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 30,fontWeight: FontWeight.w500),),
+    ],
+  );
+}
+
+
+Widget title_name(BuildContext context,{required String name}) {
+  return Row(
+    children: [
+      Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: Text(name,style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 25,fontWeight: FontWeight.w400)),
+      ),
+      Spacer(),
+      Padding(
+        padding: EdgeInsets.only(right: 20),
+        child: Text('View All',style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 15,fontWeight: FontWeight.w300)),
+      ),
+    ],
+  );
+}
+
+Padding category_grid_box(int c,{required String name}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 14.0),
+    child: SizedBox(
+      height: height! / 4.9,
+      child: GridView.builder(
+        shrinkWrap: true,
+        itemCount: c,
+        scrollDirection: Axis.horizontal,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 10,
+            mainAxisExtent: 110,
+            childAspectRatio: 50 / 130),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.only(
+                left: index == 0 ? 10 : 1.5,
+                right: index == 9 ? 10 : 1.5),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(image: AssetImage(image[name][index]),fit: BoxFit.cover)
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
---
+++ lib/view/theme screen/theme_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../utils/global_variables.dart';
+import 'componets/them_title.dart';
+
+class ThemeScreen extends StatefulWidget {
+  const ThemeScreen({super.key});
+
+  @override
+  State<ThemeScreen> createState() => _ThemeScreenState();
+}
+
+class _ThemeScreenState extends State<ThemeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ThemeTitle(context),
+              SizedBox(height: 20,),
+              title_name(context,name: 'Most Popular !'),
+              category_grid_box(10,name: 'affirmation'),
+              title_name(context,name: 'Broken'),
+              category_grid_box(10,name: 'broken'),
+              title_name(context,name: 'Deep'),
+              category_grid_box(10,name: 'deep'),
+              title_name(context,name: 'Ego'),
+              category_grid_box(10,name: 'ego'),
+              title_name(context,name: 'Friendship'),
+              category_grid_box(10,name: 'friendship'),
+              title_name(context,name: 'Funny'),
+              category_grid_box(10,name: 'funny'),
+              title_name(context,name: 'Happy'),
+              category_grid_box(10,name: 'happy'),
+              title_name(context,name: 'Hope'),
+              category_grid_box(10,name: 'hope'),
+              title_name(context,name: 'Kindness'),
+              category_grid_box(10,name: 'kindness'),
+              title_name(context,name: 'Love'),
+              category_grid_box(10,name: 'love'),
+              title_name(context,name: 'Mental Health'),
+              category_grid_box(10,name: 'mental health'),
+              title_name(context,name: 'Motivation'),
+              category_grid_box(10,name: 'motivation'),
+              title_name(context,name: 'Positive'),
+              category_grid_box(10,name: 'positive'),
+              title_name(context,name: 'Sad'),
+              category_grid_box(10,name: 'sad'),
+              title_name(context,name: 'Success'),
+              category_grid_box(10,name: 'success'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
