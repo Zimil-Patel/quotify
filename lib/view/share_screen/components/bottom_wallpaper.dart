0eef4aabfd47ce8ddbf49651384f4611361feb48
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-04-22 18:59:35 +0530

    form akshar

--- lib/view/share_screen/components/bottom_wallpaper.dart
+++ lib/view/share_screen/components/bottom_wallpaper.dart
@@ -5,6 +5,7 @@ import 'dart:ui' as ui;
 import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -16,63 +17,79 @@ Future<dynamic> wallpaper(BuildContext context) {
   return showModalBottomSheet(
     context: context,
     builder: (context) => SizedBox(
-      height: height! / 2.3,
+      height: height! / 2.8,
       width: width,
+
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 20.0, bottom: 5, left: 20),
-            child: Text('Wallpaper',style:  Theme.of(context).textTheme.titleSmall!.copyWith(
-                fontSize: 20, color: Theme.of(context).colorScheme.secondary),),
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Center(
+              child: Text('Set as wallpaper',style:  Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 20,fontWeight: FontWeight.w500),),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Divider(),
+            child: Divider(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
           ),
           Column(
             children: List.generate(
               wallpaperlist.length,
-                  (index) => CupertinoButton(
-                child: Container(
-                  height: 50,
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
+                  (index) => Column(
+                    children: [
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                                      child: Container(
+                      height: 50,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.surface,
+                      ),
+                      child: Text(wallpaperlist[index],style: Theme.of(context).textTheme.titleSmall!.copyWith(fontSize:18 ),),
+                                      ),
+                                      onPressed: () async {
+                      final boudary = userProvider!.imgKey.currentContext!
+                          .findRenderObject() as RenderRepaintBoundary;
+
+                      ui.Image image = await boudary.toImage();
+
+                      ByteData? byteData =
+                      await image.toByteData(format: ui.ImageByteFormat.png);
+
+                      final imgData = byteData!.buffer.asUint8List();
+
+                      final directory = await getApplicationDocumentsDirectory();
+
+                      fileImg = File(
+                          "${directory.path}/flutter/${DateTime.now().millisecondsSinceEpoch}.png");
+                      fileImg!.createSync(recursive: true);
+
+                      fileImg!.writeAsBytesSync(imgData);
+
+                      if (index == 0) {
+                        await setWallpaper(context, AsyncWallpaper.HOME_SCREEN);
+                      } else if (index == 1) {
+                        await setWallpaper(context, AsyncWallpaper.LOCK_SCREEN);
+                      } else if (index == 2) {
+                        await setWallpaper(context, AsyncWallpaper.BOTH_SCREENS);
+                      }
+
+                      Navigator.of(context).pop();
+                                      },
+                                    ),
+                      (index<2)?Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                        child: Divider(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ):Container(),
+                    ],
                   ),
-                  child: Text(wallpaperlist[index],style: Theme.of(context).textTheme.titleMedium,),
-                ),
-                onPressed: () async {
-                  final boudary = userProvider!.imgKey.currentContext!
-                      .findRenderObject() as RenderRepaintBoundary;
-
-                  ui.Image image = await boudary.toImage();
-
-                  ByteData? byteData =
-                  await image.toByteData(format: ui.ImageByteFormat.png);
-
-                  final imgData = byteData!.buffer.asUint8List();
-
-                  final directory = await getApplicationDocumentsDirectory();
-
-                  fileImg = File(
-                      "${directory.path}/flutter/${DateTime.now().millisecondsSinceEpoch}.png");
-                  fileImg!.createSync(recursive: true);
-
-                  fileImg!.writeAsBytesSync(imgData);
-
-                  if (index == 0) {
-                    await setWallpaper(context, AsyncWallpaper.HOME_SCREEN);
-                  } else if (index == 1) {
-                    await setWallpaper(context, AsyncWallpaper.LOCK_SCREEN);
-                  } else if (index == 2) {
-                    await setWallpaper(context, AsyncWallpaper.BOTH_SCREENS);
-                  }
-
-                  Navigator.of(context).pop();
-                },
-              ),
             ),
           )
         ],
